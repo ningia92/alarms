a6912c9405ad9f5f41abb56065528b616badb666
ningia <113545072+ningia92@users.noreply.github.com>
2025-08-06 12:09:34 +0200

    add check if the room exists in the controller of the route that activate the alarm

--- backend/controllers/alarmDevices.js
+++ backend/controllers/alarmDevices.js
@@ -7,9 +7,20 @@ const redisClient = getRedisClient();
 export const turnOnAlarm = async (req, res) => {
   const id = req.params.id;
 
+  const room = await redisClient.exists(`room:${id}`);
+
+  if (!room) {
+    const error = new Error('Room not found');
+    error.statusCode = 404;
+    throw error;
+  }
+
   await redisClient.hSet(`room:${id}:alarm`, { status: 'on' });
 
   // set these response headers to mitigate the problem of the caching caused by the unsafe GET
+  // Cache-Control: no-store indicates that any caches of any kind (private or shared) should 
+  // not store the reponse
+  // Expires: 0 ensures compatibility even with older HTTP clients
   res
     .set('Cache-Control', 'no-store')
     .set('Expires', '0')
