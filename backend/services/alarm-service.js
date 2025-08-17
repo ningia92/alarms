20305d86257324cb4ae79f4767c4a6c8b6291949
ningia <113545072+ningia92@users.noreply.github.com>
2025-08-17 13:12:54 +0200

    backend: add the lastActivation field to the alarm data in the db which represents the date and time of the last alarm activation

--- backend/services/alarm-service.js
+++ backend/services/alarm-service.js
@@ -20,14 +20,44 @@ export const setAlarmStatus = async (id, status, lastUpdate) => {
   }
 
   try {
-    await redisClient.hSet(`room:${id}:alarm`, { status: `${status}`, lastUpdate: `${lastUpdate}` });
+    await redisClient.hSet(`room:${id}:alarm`,
+      {
+        status: `${status}`,
+        lastUpdate: `${lastUpdate}`
+      });
   } catch (err) {
     console.error(`Error while setting alarm status to ${status}`, err);
+  }
+
+  // get last alarm activation
+  const lastActivation = await redisClient.hGet(`room:${id}:alarm`, 'lastActivation');
+
+  // when an alarm is turned on and the last alarm activation is empty,
+  // set the last activation time to the last update time
+  // this ensures that when the alarm is turned on, the penultimate alarm is shown and
+  // not the current one
+  if (status === 'on' && !lastActivation) {
+    try {
+      await redisClient.hSet(`room:${id}:alarm`, { lastActivation: `${lastUpdate}` });
+    } catch (err) {
+      console.error('Error while setting last alarm activation', err);
+    }
+  }
+
+  // when and alarm is turned off and the last alarm activation is not empty,
+  // set the last activation time to the last update time
+  // this ensures that the last activation is always shown when the alarm is turned off
+  if (status === 'off' && lastActivation) {
+    try {
+      await redisClient.hSet(`room:${id}:alarm`, { lastActivation: `${lastUpdate}` });
+    } catch (err) {
+      console.error('Error while setting last alarm activation', err);
+    }
   }
 
   // write logs
   const roomType = await redisClient.hGet(`room:${id}`, 'type');
   const formatDate = new Date(lastUpdate).toLocaleString();
-  const log = `${formatDate} [ ${roomType === 'room'? 'Room ' + id : 'Pool'} ] => ${status} (${status === 'on' ? 'Alarm activated' : 'Alarm deactivated'})\n`;
+  const log = `${formatDate} [ ${roomType === 'room' ? 'Room ' + id : 'Pool'} ] => ${status} (${status === 'on' ? 'Alarm activated' : 'Alarm deactivated'})\n`;
   await writeLogs(log);
 }
