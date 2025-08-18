11ec1d81cc6df49350b95ceb7c4cd9d747edb2d1
ningia <113545072+ningia92@users.noreply.github.com>
2025-08-18 18:33:09 +0200

    backend: move alarm logger function in the appropriate file and folder to increase readability and maintainability

--- backend/services/alarm-service.js
+++ backend/services/alarm-service.js
@@ -1,18 +1,18 @@
 import { getRedisClient } from '../db/redis-client.js';
-import { writeLogs } from '../utils/write-logs.js';
+import { alarmLogger } from '../utils/alarm-logger.js';
 
 const redisClient = getRedisClient();
 
 // function that writes data on Redis db and writes logs on .txt file
-export const setAlarmStatus = async (id, status, lastStatusChange) => {
+export const setAlarmStatus = async (roomId, status, timestamp) => {
   // input validation of the room id
-  if (!id.match(/^(\d)+-(\d)+$/g)) {
+  if (!roomId.match(/^(\d)+-(\d)+$/g)) {
     const error = new Error('Bad request');
     error.statusCode = 400;
     throw error;
   }
 
-  const roomExists = await redisClient.exists(`room:${id}`);
+  const roomExists = await redisClient.exists(`room:${roomId}`);
 
   if (!roomExists) {
     const error = new Error('Room not found');
@@ -20,24 +20,24 @@ export const setAlarmStatus = async (id, status, lastStatusChange) => {
     throw error;
   }
 
-  // if status is on, set lastActivation of the alarm to lastStatusChange
+  // old status of the alarm used for the logs
+  const oldStatus = await redisClient.hGet(`room:${roomId}:alarm`, 'status');
+
+  // if status is on, set lastActivation of the alarm to timestamp
   try {
     if (status === 'on') {
-      await redisClient.hSet(`room:${id}:alarm`,
+
+      await redisClient.hSet(`room:${roomId}:alarm`,
         {
           status: `${status}`,
-          lastActivation: `${lastStatusChange}`
+          lastActivation: `${timestamp}`
         });
     } else {
-      await redisClient.hSet(`room:${id}:alarm`, { status: `${status}` });
+      await redisClient.hSet(`room:${roomId}:alarm`, { status: `${status}` });
     }
   } catch (err) {
     console.error(`Error while setting alarm status to ${status}`, err);
   }
 
-  // write logs for activation/deactivation of the alarms
-  const roomType = await redisClient.hGet(`room:${id}`, 'type');
-  const formatDate = new Date(lastStatusChange).toLocaleString();
-  const log = `${formatDate} [ ${roomType === 'room' ? 'Room ' + id : 'Pool'} ] => ${status} (${status === 'on' ? 'Alarm activated' : 'Alarm deactivated'})\n`;
-  await writeLogs(log);
+  await alarmLogger(roomId, oldStatus, status, timestamp);
 }
---
+++ backend/utils/alarm-logger.js
@@ -0,0 +1,32 @@
+import fs from 'fs/promises';
+import { getRedisClient } from '../db/redis-client.js';
+
+const redisClient = getRedisClient();
+
+const writeFile = async (data) => {
+  try {
+    // because the server is launched from top-level directory, the file
+    // path needs to start with single dot "./" and not double dot "../"
+    fs.writeFile('./logs/alarm-logs.txt', data, { flag: 'a+' });
+  } catch (err) {
+    console.error('Error during write logs', err);
+  }
+}
+
+// function that handles writing of logs
+export const alarmLogger = async (id, oldStatus = '', status, timestamp) => {
+  const roomType = await redisClient.hGet(`room:${id}`, 'type');
+  const formatDate = new Date(timestamp).toLocaleString();
+  let description = '';
+
+  if (status === 'on' && oldStatus === 'on') {
+    description = 'Alarm activation repeated';
+  } else if (status === 'on') {
+    description = 'Alarm activated';
+  } else if (status === 'off') {
+    description = 'Alarm deactivated';
+  }
+
+  const log = `${formatDate} [ ${roomType === 'room' ? 'Room ' + id : 'Pool'} ] => ${status} (${description})\n`;
+  await writeFile(log);
+}
