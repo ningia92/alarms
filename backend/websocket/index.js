029a416af2a0512a935ea926eaf19b7dadbf8fdb
ningia <113545072+ningia92@users.noreply.github.com>
2025-08-25 11:25:03 +0200

    backend: add the functionality that detects and closes broken web socket connections

--- backend/websocket/index.js
+++ backend/websocket/index.js
@@ -5,8 +5,16 @@ import { handleAlarmOff } from './handlers/alarm-handler.js';
 export const initializeWebSocketServer = (server) => {
   const wss = new WebSocketServer({ server });
 
+  // if this function is called it means that the connection to client is up
+  function heartBeat() {
+    this.isAlive = true;
+  }
+
   wss.on('connection', async (ws) => {
     console.log('WebSocket client connected');
+    // set the initial state of client and handle the ping response
+    ws.isAlive = true;
+    ws.on('pong', heartBeat);
 
     // send room list to the newly connected web socket client
     try {
@@ -35,6 +43,26 @@ export const initializeWebSocketServer = (server) => {
       console.log('WebSocket client disconnected');
     });
   });
+
+  // terminate the broken connections for dead clients and
+  // send a 'ping' for the others, if they repond with a 'pong'
+  // the event 'pong' will return isAlive to true with the function heartBeat
+  const ping = () => {
+    wss.clients.forEach(ws => {
+      if (ws.isAlive === false) return ws.terminate();
+
+      ws.isAlive = false;
+      ws.ping();
+    })
+  }
+
+  // set the interval for the ping call to 3 seconds
+  const interval = setInterval(ping, 3000);
+
+  // clean the interval when the server is closed
+  wss.on('close', () => {
+    clearInterval(interval);
+  })
 
   return wss;
 }
