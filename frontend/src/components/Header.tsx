a3387ede0bd3f2324c89ccf8fe3f1c0dbcca5833
ningia <113545072+ningia92@users.noreply.github.com>
2025-09-02 12:40:58 +0200

    frontend: minor changes

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -13,12 +13,10 @@ const Header: React.FC<HeaderProps> = ({ theme, toggleTheme }) => {
     <header className="sticky top-0 z-50 bg-white/80 dark:bg-zinc-900/70 backdrop-blur-sm border-b border-zinc-200 dark:border-zinc-800">
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <div className='text-2xl font-bold text-zinc-800 dark:text-zinc-100 flex items-center gap-4'>
-            <img src={logo} alt="VOI Hotels logo" className="h-8 w-auto" />
-            <h3 style={{ color: "#7889aa" }} className="text-2xl font-bold text-zinc-800 dark:text-zinc-100">
-              Dashboard Allarmi
-            </h3>
-          </div>
+          <img src={logo} alt="VOI Hotels logo" className="h-8 w-auto" />
+          <h1 style={{ color: "#7889aa" }} className="text-2xl font-bold">
+            Dashboard Allarmi
+          </h1>
           <button
             onClick={toggleTheme}
             className="p-2 rounded-full text-zinc-500 dark:text-zinc-400 hover:bg-zinc-100 dark:hover:bg-zinc-700 focus:outline-none"
